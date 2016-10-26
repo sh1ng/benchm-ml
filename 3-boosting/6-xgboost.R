b19be38124cf5e1b23cfe6b8b8c4be142a8b896d
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2016-10-26 23:50:25 +0200

    no message

--- 3-boosting/6-xgboost.R
+++ 3-boosting/6-xgboost.R
@@ -22,7 +22,7 @@ dim(X_train)
 dxgb_train <- xgb.DMatrix(data = X_train, label = ifelse(d_train$dep_delayed_15min=='Y',1,0))
 dxgb_test  <- xgb.DMatrix(data = X_test,  label = ifelse(d_test$dep_delayed_15min =='Y',1,0))
 
-rm(X_train, X_test, d_train, d_test)
+rm(X_train, d_train)
 
 
 
